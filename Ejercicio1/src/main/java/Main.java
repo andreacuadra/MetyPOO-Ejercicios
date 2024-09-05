900e91d96361d898d5ad8a631d464067cb9e18bc
andrea cuadra <acuadrap@uamv.edu.ni>
2024-09-05 15:57:49 -0600

    Primer commit

---
+++ Ejercicio1/src/main/java/Main.java
@@ -0,0 +1,26 @@
+import models.student;
+
+public class Main {
+
+    public static void main (String[] args){
+
+        import models.Admision;
+import models.Student;
+
+import java.util.Scanner;
+
+        public class main {
+            public static void main(String[] args) {
+                Student student = new Student();
+                Admision admision = new Admision();
+                Scanner sc = new Scanner(System.in);
+                float promedio=0;
+                System.out.println("Ingrese el promedio del estudiante: ");
+                promedio=sc.nextFloat();
+                admision.calSchoolarship(student);
+                System.out.println();
+}
+        }
+    }
+
+}
